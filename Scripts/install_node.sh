e1d95a49e980f986fa4c1d00063f0085ce2115af
Adriano Rosa <adrianorosa02@prof.educacao.sp.gov.br>
2026-04-13 09:12:31 -0300

    chore: Atualizado script para instalação nvm

--- Scripts/install_node.sh
+++ Scripts/install_node.sh
@@ -3,10 +3,18 @@
 # Autor: [Adriano Rosa] - [13/04/2026]
 
 # Define local para execução da instalação do NVM
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#!/bin/bash
 
-# Baixa o script de instalação do NVM e executa
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash/
-echo "NVM instalado com sucesso!"
+export NVM_DIR="$HOME/.nvm"
+
+if [ ! -d "$NVM_DIR" ]; then
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
+
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+nvm install 24
+nvm use 24
+
+node -v
+npm -v
